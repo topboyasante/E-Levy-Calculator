bd965ceb4cee7c4d54a36bdec4a5ee28ce467630
Nana K <topboyasante@gmail.com>
2022-07-09 15:34:47 +0000

    new

--- script.js
+++ script.js
@@ -7,31 +7,31 @@ function returnText(){
     let amtToRecipient = AmtSending;
 
        if(AmtSending<=50 && network=='MTN'){
-            mtnCharge = 0.38;
-            eLevy = 0*AmtSending;
+            mtnCharge = 0;
+            eLevy = (1.5/100)*AmtSending;
             totalCharge = mtnCharge+eLevy;
-            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.floor(totalCharge)}.
-            Hence You'd Spend GHC${Math.floor(amtToRecipient+totalCharge)}`);
+            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.ceil(totalCharge)}.
+            Hence You'd Spend GHC${Math.ceil(amtToRecipient+totalCharge)}`);
        }else if(50<AmtSending<=1000 && network=='MTN'){
             mtnCharge = (0.75/100)*AmtSending;
             eLevy = (1.50/100)*AmtSending;
             totalCharge = mtnCharge+eLevy;
-            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.floor(totalCharge)}.
-            Hence You'd Spend GHC${Math.floor(amtToRecipient+totalCharge)}`);
+            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.ceil(totalCharge)}.
+            Hence You'd Spend GHC${Math.ceil(amtToRecipient+totalCharge)}`);
        }
 
        if(AmtSending<=50 && network == 'VODAFONE'){
             vfCharge = 0;
             eLevy = 0*AmtSending;
             totalCharge = vfCharge+eLevy;
-            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.floor(totalCharge)}.
-            Hence You'd Spend GHC${Math.floor(amtToRecipient+totalCharge)}`);
+            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.ceil(totalCharge)}.
+            Hence You'd Spend GHC${Math.ceil(amtToRecipient+totalCharge)}`);
        }else if(50<AmtSending<=1000 && network=='VODAFONE'){
             vfCharge = (0.75/100)*AmtSending;
             eLevy = (1.50/100)*AmtSending;
             totalCharge = vfCharge+eLevy;
-            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.floor(totalCharge)}.
-            Hence You'd Spend GHC${Math.floor(amtToRecipient+totalCharge)}`);
+            document.getElementById('totalAmt').innerHTML=(`Recipient Gets GHC${amtToRecipient}, and the total charges are GHC${Math.ceil(totalCharge)}.
+            Hence You'd Spend GHC${Math.ceil(amtToRecipient+totalCharge)}`);
          }
       
 }
